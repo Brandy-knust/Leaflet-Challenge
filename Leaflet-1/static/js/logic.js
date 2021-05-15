c3aecf28cace38473f5145dfd43a5739548bbfef
Brandy Knust <brandy_1175@hotmail.com>
2021-05-15 15:50:20 -0500

    4pm 5-15

--- Leaflet-1/static/js/logic.js
+++ Leaflet-1/static/js/logic.js
@@ -6,14 +6,20 @@ d3.json(queryUrl).then(function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
   createFeatures(data.features);
 });
-
-function createFeatures(earthquakeData) {
+var earthquakes = []
+function createFeatures(data) {
 
   // Define a function we want to run once for each feature in the features array
   // Give each feature a popup describing the place and time of the earthquake
+  var markers= []
+  earthquakes.forEach(earthquake => {
+    markers.push(L.marker([geometry.coordinates[0,1]]))
+    markersize.push(L.markersize([properties.mag]))
+    markercolor.push(L.markercolor([geometry.coordinates[2]]))
+    .bindPopup("<h3>" + properties.place + "<br>" + properties.date + "</br></h3><hr>");
+  })
   function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    layer
   }
 
   // Create a GeoJSON layer containing the features array on the earthquakeData object
@@ -46,4 +52,21 @@ var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
     "Street Map": streetmap,
     "Dark Map": darkmap
   };
+
+  var overlayMaps = {
+    Earthquakes: earthquakes
+  }; 
+
+  var myMap = L.map("mapid", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [streetmap, darkmap, earthquakes]
+  });
+
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
   
