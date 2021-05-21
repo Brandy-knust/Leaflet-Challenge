02bfd86cfca721a8eb7cf4fb566b5115bc4aae1e
Brandy Knust <brandy_1175@hotmail.com>
2021-05-21 09:29:30 -0500

    930am 5-21

--- Leaflet-1/static/js/logic.js
+++ Leaflet-1/static/js/logic.js
@@ -8,91 +8,104 @@ d3.json(queryUrl).then(function (data) {
   console.log(`feature:${data.features[0].properties.mag}`)
   createFeatures(data.features);
 });
-var earthquakes = []
 function createFeatures(earthquakeData) {
 
   // Define a function we want to run once for each feature in the features array
   // Give each feature a popup describing the place and time of the earthquake
   // var markers = []
-  earthquakeData.forEach(earthquake => {
+  function markerColor(depth) {
+    // earthquakeData.forEach(earthquake => {
     var color = "";
-    
-    console.log(`coordinates ${earthquake}`)
-    if (earthquake.geometry.coordinates[2] > 90) {
-      color = "red";
+
+    // console.log(`coordinates ${earthquake}`)
+    // if (earthquake.geometry.coordinates[2] > 90) {
+    //   color = "red";
+    // }
+    if (depth > 90) {
+      color = "#d92b2b"
     }
-    else if (earthquake.geometry.coordinates[2] > 70) {
-      color = "orange";
+    else if (depth > 70) {
+      color = "#f0873c";
     }
-    else if (earthquake.geometry.coordinates[2] > 50) {
-      color = "yellow";
+    else if (depth > 50) {
+      color = "#ed7f3b";
     }
-    else if (earthquake.geometry.coordinates[2] > 30) {
-      color = "lime";
+    else if (depth > 30) {
+      color = "#edb63e";
+    }
+    else if (depth > 10) {
+      color = "#beed3e";
     }
     else {
-      color = "green";
+      color = "#8aed3e";
     }
+    return color;
+    // console.log(`color: ${color}`)
+  }
 
-    console.log(`color: ${color}`)
+function pointToLayer (earthquake, coords) {
+  return L.circleMarker(coords, {
+    fillOpacity: 0.75,
+    color: "white",
+    fillColor: markerColor(earthquake.geometry.coordinates[2]),
+    weight: 1,
+    opacity: 0.7,
+    // Adjust radius
+    radius: (earthquake.properties.mag) *3
+  })}
+  function onEachFeature (earthquake, popUp) {
+    popUp.bindPopup("<h3>" + earthquake.properties.place +
+    "</h3><hr><p>" + (earthquake.properties.mag) + "</p>")
+  };
 
-    L.circle(earthquake.geometry.coordinates, {
-      fillOpacity: 0.75,
-      color: "white",
-      fillColor: color,
-      // Adjust radius
-      radius: (earthquake.properties.mag) * 1200
-    }).bindPopup("<h3>" + earthquake.properties.place +
-      "</h3><hr><p>" + (earthquakeData.properties.mag) + "</p>");
 
-    
 
-    // Create a GeoJSON layer containing the features array on the earthquakeData object
-    // Run the onEachFeature function once for each piece of data in the array
-    var earthquakes = L.geoJSON(earthquakeData, {
-      onEachFeature: onEachFeature
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    pointToLayer: pointToLayer,
+    onEachFeature: onEachFeature
+  })
+  createMap(earthquakes);
+}
+  // Sending our earthquakes layer to the createMap function
+  
+  function createMap(earthquakes) {
+
+    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      tileSize: 512,
+      maxZoom: 18,
+      zoomOffset: -1,
+      id: "mapbox/streets-v11",
+      accessToken: API_KEY
     });
 
-    // Sending our earthquakes layer to the createMap function
-    createMap(earthquakes);
+    var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/brandy1175/ckoydzai1111s17o0mex7bf6x.html?fresh=true&title=view&access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "satellite-streets-v11",
+      accessToken: API_KEY
+    });
 
-    function createMap(earthquakes) {
+    var baseMaps = {
+      "Street Map": streetmap,
+      "Satellite": satellite
+    };
 
-      var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-        tileSize: 512,
-        maxZoom: 18,
-        zoomOffset: -1,
-        id: "mapbox/streets-v11",
-        accessToken: API_KEY
-      });
+    var overlayMaps = {
+      Earthquakes: earthquakes
+    };
 
-      var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-        maxZoom: 18,
-        id: "dark-v10",
-        accessToken: API_KEY
-      });
+    var myMap = L.map("mapid", {
+      center: [
+        10, 0
+      ],
+      zoom: 3,
+      layers: [streetmap, satellite, earthquakes]
+    });
 
-      var baseMaps = {
-        "Street Map": streetmap,
-        "Dark Map": darkmap
-      };
-
-      var overlayMaps = {
-        Earthquakes: earthquakes
-      };
-
-      var myMap = L.map("mapid", {
-        center: [
-          37.09, -95.71
-        ],
-        zoom: 5,
-        layers: [streetmap, darkmap, earthquakes]
-      });
-
-      L.control.layers(baseMaps, overlayMaps, {
-        collapsed: false
-      }).addTo(myMap);
-    }
-  })}
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(myMap);
+  }
